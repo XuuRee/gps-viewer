8c33fbf89dffad8acfa3fde064eb342f8694fb1e
Filip Valchář <xvalchar@fi.muni.cz>
2018-04-29 10:52:06 +0200

    done, not abstrac class

--- PV178.Homeworks.HW05/Program.cs
+++ PV178.Homeworks.HW05/Program.cs
@@ -14,16 +14,16 @@ namespace PV178.Homeworks.HW05
         {
             //string json = @"C:\Users\Filip_000\Desktop\route01.json";
             //string gpx = @"C:\Users\Filip_000\Desktop\route02.gpx";
-            //string fuzzy1 = @"C:\Users\Filip_000\Desktop\route03.ff1";
-            string fuzzy2 = @"C:\Users\Filip_000\Desktop\route04.ff2";
+            string fuzzy1 = @"C:\Users\Filip_000\Desktop\route03.ff1";
+            //string fuzzy2 = @"C:\Users\Filip_000\Desktop\route04.ff2";
 
-            FuzzyFormat2Parser parser = new FuzzyFormat2Parser();
-            Track result = parser.ParseTrack(fuzzy2);
-            Console.WriteLine("Items: {0}", result.GPSPoints.Count);
-
-            //FuzzyFormat1Parser parser = new FuzzyFormat1Parser();
-            //Track result = parser.ParseTrack(fuzzy1);
+            //FuzzyFormat2Parser parser = new FuzzyFormat2Parser();
+            //Track result = parser.ParseTrack(fuzzy2);
             //Console.WriteLine("Items: {0}", result.GPSPoints.Count);
+
+            FuzzyFormat1Parser parser = new FuzzyFormat1Parser();
+            Track result = parser.ParseTrack(fuzzy1);
+            Console.WriteLine("Items: {0}", result.GPSPoints.Count);
 
             //GpxXmlParser parser = new GpxXmlParser();
             //Track result = parser.ParseTrack(gpx);
